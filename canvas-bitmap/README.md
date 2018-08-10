f9648fa11d657b62448864ce21dcd61d7357d822
chenyong5 <chenyong5@xiaomi.com>
2018-08-10 15:10:58 +0800

    update

---
+++ canvas-bitmap/README.md
@@ -0,0 +1,2 @@
+### 修改了wxhelper.js GetWindowSizeInPx（）返回宽高在移动端形成滚动问题
+### 引入stat
---
+++ canvas-bitmap/test.js
@@ -0,0 +1,9 @@
+(function(){
+  function init(){
+    var canvas = document.querySelector('canvas')
+    console.log('---------', canvas)
+  }
+  window.onload = function(){
+      init()
+  }
+})()
