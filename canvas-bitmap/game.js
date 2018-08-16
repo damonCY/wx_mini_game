40e9a9764bc8a585e77a9fbfe6693e310fd3ac71
chenyong5 <chenyong5@xiaomi.com>
2018-08-16 10:30:33 +0800

    update webgl-compute

--- canvas-bitmap/game.js
+++ canvas-bitmap/game.js
@@ -1,3 +1,4 @@
 import "./wxhelper.js"
-
+import './Stats.js'
 import "./guimark3.js"
+// import "./test.js"
--- canvas-bitmap/test.js
+++ canvas-bitmap/test.js
@@ -1,9 +1,32 @@
-(function(){
-  function init(){
-    var canvas = document.querySelector('canvas')
-    console.log('---------', canvas)
+var wx = wx || ''
+var showFPS = (function (wx) {
+  if(!wx){return}
+  var requestAnimationFrame = window.requestAnimationFrame;
+
+  var fps =0; var offset = 0; var last = 0;
+  var last = Date.now();
+ var  step = function () {
+    fps += 1
+    offset = Date.now() - last;
+    if (offset >= 10000) {
+      last += offset;
+      appendFPS(fps)
+      fps = 0;
+    }
+    requestAnimationFrame(step);
   }
-  window.onload = function(){
-      init()
+  function appendFPS(fps) {
+    wx.showToast({
+      title: 'fps: ' + (fps/10),
+      icon: 'loading',
+      duration: 500
+    })
+  };
+  return {
+    step: step
   }
-})()
+})(wx)
+if(wx) {
+  showFPS.step(wx)
+}
+
